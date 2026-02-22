012fcd6088a95d780bb1873ed88fc1e1c618bfb7
mahindraj458-star <mahindraj458@gmail.com>
2026-02-22 15:14:26 +0530

    blog_data.js

---
+++ blog_data (3).js
@@ -0,0 +1,10 @@
+window.blogPosts = [
+  {
+    "id": 1771753367783,
+    "title": "dasasd",
+    "excerpt": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuQ-qXCZ6YA4VvlMDn38LPWyDYa9iYBx0N7Q&s",
+    "content": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuQ-qXCZ6YA4VvlMDn38LPWyDYa9iYBx0N7Q&s",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuQ-qXCZ6YA4VvlMDn38LPWyDYa9iYBx0N7Q&s",
+    "date": "22/02/2026"
+  }
+];
