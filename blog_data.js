951201ee4fe78b75a33f2a056221044a470a87eb
mahindraj458-star <mahindraj458@gmail.com>
2026-02-22 15:07:49 +0530

    Add files via upload

---
+++ blog_data.js
@@ -0,0 +1,10 @@
+window.blogPosts = [
+  {
+    "id": 1771752962373,
+    "title": "mksksk",
+    "excerpt": "weqadada",
+    "content": "asdasdasdasda",
+    "image": "https://cdn-imgix.headout.com/media/images/c9db3cea62133b6a6bb70597326b4a34-388-dubai-img-worlds-of-adventure-tickets-01.jpg?auto=format&w=1222.3999999999999&h=687.6&q=90&ar=16%3A9&crop=faces&fit=crop",
+    "date": "22/02/2026"
+  }
+];
