2e9bad8fcda08b26c70a5631c21d34215d9af915
mahindraj458-star <mahindraj458@gmail.com>
2026-02-24 09:56:50 +0530

    Add files via upload

---
+++ dat.js
@@ -0,0 +1,28 @@
+/* APEX SHOWROOM DATA // EXPORTED 24/02/2026, 09:56:34 */
+
+window.carDatabase = [
+    {
+        "id": "CAR-1771907190845",
+        "name": "Toyota AE86 Sprinter Trueno",
+        "marketStatus": "Discontinued / Collector Only",
+        "year": "1983–1987",
+        "origin": "Toyota City, Aichi, Japan",
+        "images": {
+            "desktop": "https://i.postimg.cc/X77TSYH6/Chat-GPT-Image-Feb-24-2026-09-37-33-AM.png",
+            "mobile": "https://i.postimg.cc/8zx36B1V/Chat-GPT-Image-Feb-24-2026-09-38-51-AM.png"
+        },
+        "model3D": "https://raw.githubusercontent.com/mahindraj458-star/apex-assets./main/1985_toyota_sprinter_trueno_ae86_project_d.glb",
+        "specs": {
+            "engine": "1.6L 4A-GE Inline-4 (DOHC 16-Valve)",
+            "hp": "128 HP (JDM Spec)",
+            "topSpeed": "124 MPH (200 km/h)",
+            "production": "360,000 units"
+        },
+        "legacy": {
+            "specialReason": "One of the last lightweight rear-wheel-drive compact cars of the 1980s — later immortalized as the definitive drift icon.",
+            "story": "The Toyota AE86 Sprinter Trueno was born during a technological shift where most compact platforms moved to front-wheel drive. Toyota chose to preserve the rear-wheel-drive formula for this chassis, unintentionally creating one of the most influential enthusiast cars in history.\n\nPowered by the high-revving 4A-GE engine and weighing under 1,000 kg, the AE86 delivered pure mechanical feedback. Its balanced chassis and predictable oversteer made it a natural fit for touge racing and grassroots drifting in Japan.\n\nIts global cult status exploded through Initial D, where the panda-liveried Trueno became a symbol of driver skill over horsepower. From mountain roads to professional drift circuits, the AE86 evolved from an affordable coupe into a collector-grade icon.\n\nToday, surviving examples are highly sought after, restored, modified, and preserved as artifacts of analog performance engineering.",
+            "mediaList": "Initial D(Manga series)",
+            "mediaImage": "https://m.media-amazon.com/images/S/pv-target-images/c289ded1d9a2f66b5f3e5ab21b3d5aad0ead7d1161249ddf83a580f8d33a7e3a.jpg"
+        }
+    }
+];
